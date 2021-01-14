cfdbd06fe29d39d00d4b663693bf4156f7ec4f9a
Joe Meyer <j23meyerj@gmail.com>
2021-01-14 13:58:53 -0600

    saving get.

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,5 +4,18 @@ const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
 
+router.get('/', (req, res) => {
+    const sqlText = `SELECT * FROM shoplist;`;
+    pool.query(sqlText)
+    .then((result) => {
+        console.log('from db', result);
+        res.send(result.rows);
+    })
+    .catch((error) => {
+        console.log(`err with db query ${sqlText}`, error);
+        res.sendStatus(500);
+    })
+
+})
 
 module.exports = router;
