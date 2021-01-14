629669ab0cca6ddd19e3ce656fd14a224368a596
NateMacLaurin <brow1982@umn.edu>
2021-01-14 14:29:49 -0600

    added post route and post function in app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
-
-import Header from '../Header/Header.jsx'
+import React, {useState, useEffect} from 'react';
+import Header from '../Header/Header.jsx';
 import './App.css';
-
+//import ListForm from '../ListForm/ListForm.jsx';
 
 function App() {
+
+    let [itemName, setItemName] = useState('');
+    let [itemQuantity, setItemQuantity] = useState(0);
+    let [itemUnit, setItemUnit] = useState('');
+    //let [listArray, setListArray] = useState([]);
+
+    const addPerson = (evt) => {
+        evt.preventDefault();
+        console.log( `The person is ${famousPersonName} and they're famous for ${famousPersonRole}` );
+        
+        // create POST request to add this new person to the database
+        axios.post('/list', {
+            name: itemName,
+            quantity: itemQuantity,
+            unit: itemUnit
+        }).then((response) => {
+            console.log('Response:', response.data);
+            //get render function
+            setItemName('');
+            setItemQuantity(0);
+            setItemUnit('');
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+
     return (
         <div className="App">
             <Header />
