4ca9c16a3a1d549f1caf6a3b7912a18c74e9f28a
NateMacLaurin <brow1982@umn.edu>
2021-01-14 09:56:35 -0600

    created database file

--- database.sql
+++ database.sql
@@ -1,2 +1,34 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+--DROP TABLE "shoplist";
+
+-- create
+CREATE TABLE "shoplist" (
+	"id" SERIAL PRIMARY KEY,
+	"name" varchar(80) NOT NULL,
+	"quantity" DECIMAL(4,2) DEFAULT 0,
+	"unit" varchar(20) NOT NULL
+);
+
+-- GET
+SELECT * FROM "shoplist";
+
+SELECT * FROM "shoplist"
+WHERE "name" ILIKE '%donut%';
+
+-- POST
+INSERT INTO "shoplist" ("name", "quantity", "unit")
+VALUES ('Bread', 1 ,'Loaf');
+VALUES ('Coffee', 1 ,'lbs');
+VALUES ('Chocolate', 6 ,'oz');
+VALUES ('Milk', 1 ,'Gal');
+VALUES ('Rosemary', 10, 'g');
+
+-- DELETE
+DELETE FROM "shoplist"
+WHERE "id" = 2;
+
+-- PUT
+UPDATE "shoplist"
+SET "name" = ''
+WHERE "id" = 6;
