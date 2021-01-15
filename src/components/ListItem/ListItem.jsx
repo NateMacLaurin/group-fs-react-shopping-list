1e350be149bea97abe5b2f7b38242d5cb21b1763
NateMacLaurin <brow1982@umn.edu>
2021-01-15 11:14:43 -0600

    reset button attempt

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,8 +1,22 @@
-import { useState } from 'react';
 import axios from 'axios';
 import './ListItem.css';
+import { useState, useEffect } from 'react';
 
-function ListItem({ item, fetchList }) {
+function ListItem({ item, fetchList, reset}) {
+    
+    const [isBought, setIsBought] = useState(false);
+    
+    useEffect( () => {
+        resetBought();
+    }, [])
+
+    const resetBought = () => {
+        if(reset){
+            if(isBought){
+            setIsBought(!isBought);
+            }
+        }
+    }
 
     const deleteItem = () => {
         console.log('clicked Remove');
@@ -17,22 +31,21 @@ function ListItem({ item, fetchList }) {
             })
     } // end deleteItem
 
-    const [isBought, setIsBought] = useState(true);
-
     const markBought = () => {
         console.log('clicked Buy', isBought);
         setIsBought(!isBought);
     } // end markBought
 
-    console.log('logging item:', item);
+    console.log(`logging item: ${item} Reset Flag: ${reset} Bought value: ${isBought}`);
 
     return (
         <div key={item.id} className="itemBox">
             <h3>{item.name}</h3>
             <p>{item.quantity} {item.unit}</p>
             {isBought 
-                ? <span><button onClick={markBought}>Buy</button> <button onClick={deleteItem}>Remove</button></span>
-                : <span>Purchased</span>}
+                ? <span>Purchased</span>
+                : <span><button onClick={markBought}>Buy</button> <button onClick={deleteItem}>Remove</button></span>
+            }
         </div>  
     )
 } // end ListItem
--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,8 +1,12 @@
 import ListItem from '../ListItem/ListItem';
 import "./ShoppingList.css";
 import axios from 'axios';
+import { useState } from 'react';
 
 function ShoppingList({ shoppingList, fetchList }) {
+    
+    const [reset, setReset] = useState(false);
+
     console.log('logging shopping list', shoppingList);
 
     const deleteRows = () => {
@@ -15,12 +19,19 @@ function ShoppingList({ shoppingList, fetchList }) {
             console.log(err);
         })
      }
+     //if i hit reset
+     //each list item should check bought state, if bought -> flip back to unbought
+
+     const resetBought = () => {
+         console.log(`Reset Bought clicked`);
+         setReset(true);
+     }
 
     return (
         <div>
             <div className="listHead">
                 <h1>Shopping List</h1>
-                <button>Reset</button><button onClick = {deleteRows}>Clear</button>
+                <button onClick = {resetBought}>Reset</button><button onClick = {deleteRows}>Clear</button>
             </div>
             <div className="listContainer">
                 {shoppingList.map(item =>
@@ -28,6 +39,7 @@ function ShoppingList({ shoppingList, fetchList }) {
                     key={item.id}
                     item={item}
                     fetchList={fetchList}
+                    reset={reset}
                 />)
                 )}
             </div>
